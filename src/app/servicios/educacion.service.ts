8ba83a4b2644a8d21812275ba1013745cc37e96b
eescalada <87399264+eescalada@users.noreply.github.com>
2022-10-26 20:46:17 -0300

    COREGI ERROR DE URL

--- src/app/servicios/educacion.service.ts
+++ src/app/servicios/educacion.service.ts
@@ -8,7 +8,7 @@ import { Educacion } from '../model/educacion';
   providedIn: 'root'
 })
 export class EducacionService {
-  expURL = 'https://backendelisolesc.herokuapp.com//educacion/';
+  expURL = 'https://backendelisolesc.herokuapp.com/educacion/';
 
   constructor(private httpClient: HttpClient) { }
 
